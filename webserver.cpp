4175cb5e0a7613e1cbd3238f28d8253bfedf06cd
Mike Moss <mrmoss@alaska.edu>
2014-04-02 15:10:25 -0800

    Fixed minor bug in webserver.cpp.

--- webserver.cpp
+++ webserver.cpp
@@ -1,6 +1,6 @@
 //Web Server Source
 //	Created By:		Mike Moss
-//	Modified On:	03/30/2014
+//	Modified On:	04/02/2014
 
 //Required Libraries:
 //	Ws2_32 (windows only)
@@ -133,7 +133,7 @@ void msl::webserver::close()
 void msl::webserver::service_client(msl::socket& client,const std::string& message)
 {
 	//If User Options Fail
-	if(user_service_client==NULL||!user_service_client(client,message))
+	if(_user_service_client==NULL||!_user_service_client(client,message))
 	{
 		//Get Requests
 		if(msl::starts_with(message,"GET"))
